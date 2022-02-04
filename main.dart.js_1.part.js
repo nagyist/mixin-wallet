13eddb5375acc0bdd06c2b2fca56b77313a00843
crossle <crossle@users.noreply.github.com>
2022-02-04 14:03:05 +0000

    deploy: cb2364561d9ad99e02cbb9fffde15303c0d0747c

---
+++ main.dart.js_1.part.js
@@ -0,0 +1,42 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B={
+b3P(d){return C.e(d)+" has been hidden"},
+b3R(d){return C.e(d)+" has been shown"},
+b3X(d){return"Average arrival time:"+C.e(d)},
+b3Y(d){return C.e(d)+" BTC"},
+b3Z(d){return"Deposit will arrive after at least "+C.e(d)+" block confirmations"},
+b40(d){return"Both a Memo and an Address are required to successfully deposit your "+C.e(d)+" to your account."},
+b42(d){return"Deposit at least "+C.e(d)+" for the first time"},
+b44(d){return"This address only supports "+C.e(d)+"."},
+b46(d,e){return C.e(d)+"/"+C.e(e)+" confirmations"},
+b48(d){return"Swap with slippage greater than "+C.e(d)+" is not currently supported"},
+b3S(d){return"value now "+C.e(d)},
+b3U(d){return"value then "+C.e(d)},
+aSS(d){return C.a3(["addAddress",A.x("Add address"),"addAddressLabelHint",A.x("Label (e.g. exchanges, wallets, etc.)"),"addAddressMemo",A.x("Destination Tag or ID Number or notes. If not, you can set it "),"addAddressMemoAction",A.x("No Memo"),"addAddressNoMemo",A.x("If you are required to fill in Destination Tag or ID Number or notes, you can "),"addAddressNoMemoAction",A.x("Add Memo"),"addAddressNoTagAction",A.x("Add Tag"),"addAddressNotSupportTip",A.x("Mixin does not support withdrawal to the"),"addAddressTagAction",A.x("No Tag"),"address",A.x("Address"),"addressSearchHint",A.x("Label, Address"),"allTransactions",A.x("All Transactions"),"alreadyHidden",B.bcQ(),"alreadyShown",B.bcR(),"amount",A.x("Amount"),"assetTrending",A.x("Trending"),"assetType",A.x("Asset Type"),"assets",A.x("Assets"),"authHint",A.x("Read-only authorization cannot use your assets, please rest assured"),"authSlogan",A.x("Mixin Wallet is a user-friendly, secure and powerful multi-chain digital wallet."),"authTips",A.x("An open source cryptocurrency wallet"),"authorize",A.x("Authorize"),"averageArrival",B.bcU(),"balance",A.x("BALANCE"),"balanceOfBtc",B.bcV(),"buy",A.x("Buy"),"buyDisclaimer",A.x("Services provided by https://sendwyre.com"),"cancel",A.x("Cancel"),"chain",A.x("Chain"),"comingSoon",A.x("Coming soon"),"completed",A.x("Completed"),"contact",A.x("Contact"),"contactReadFailed",A.x("Failed to read contact list"),"contactSearchHint",A.x("Name, Mixin ID"),"contract",A.x("Asset Key"),"copyToClipboard",A.x("Copied to Clipboard"),"delete",A.x("Delete"),"deposit",A.x("Deposit"),"depositConfirmation",B.bcW(),"depositMemoNotice",A.x("Memo is required, or you will lose your coins."),"depositNotice",B.bcX(),"depositReserve",B.bcY(),"depositTip",B.bcZ(),"depositTipBtc",A.x("This address only supports BTC."),"depositTipEos",A.x("This address supports all base on EOS tokens."),"depositTipEth",A.x("This address supports all ERC-20 tokens."),"depositTipNotSupportContract",A.x("Do not support smart contract transfers."),"depositTipTron",A.x("This address supports all TRC-10 and TRC-20 tokens, such as TRX, USDT-TRON, etc."),"depositing",A.x("Depositing"),"dontShowAgain",A.x("Don't show again"),"emptyAmount",A.x("Empty amount"),"emptyLabelOrAddress",A.x("Empty address or label"),"eosContractAddress",A.x("EOS contract address"),"errorNoCamera",A.x("No camera"),"fee",A.x("Fee"),"filterAll",A.x("All"),"filterApply",A.x("Apply"),"filterBy",A.x("FILTER BY"),"filterTitle",A.x("Filter"),"from",A.x("From"),"goPay",A.x("Go pay"),"gotIt",A.x("Got it"),"hiddenAssets",A.x("Hidden Assets"),"hide",A.x("Hide"),"hideSmallAssets",A.x("Hide small assets"),"incomplete",A.x("Incomplete"),"memo",A.x("Memo"),"memoHint",A.x("Memo"),"minerFee",A.x("Miner Fee"),"minimumReserve",A.x("Minimum reserve:"),"minimumWithdrawal",A.x("Minimum withdrawal:"),"mixinWallet",A.x("Mixin Wallet"),"networkFee",A.x("Network fee:"),"networkFeeTip",A.x("Charged by third party service provider. Paid directly to Ethereum miners to ensure transactions are completed on Ethereum. Network fees vary based on immediate market conditions."),"networkType",A.x("Network type"),"noAsset",A.x("NO ASSET"),"noResult",A.x("NO RESULT"),"noTransaction",A.x("NO TRANSACTION"),"noWithdrawalDestinationSelected",A.x("No contact or address selected"),"none",A.x("N/A"),"notMeetMinimumAmount",A.x("Does not meet minimum transaction size"),"notice",A.x("Notice"),"ok",A.x("OK"),"other",A.x("Other"),"paid",A.x("Paid"),"paidInMixin",A.x("Have you paid in Mixin?"),"paidInMixinWarning",A.x("If you have paid via Mixin, please be patient."),"pay",A.x("Pay"),"pendingConfirmations",B.bd_(),"raw",A.x("Raw"),"reauthorize",A.x("Reauthorize"),"rebate",A.x("Rebate"),"receive",A.x("Receive"),"received",A.x("Received"),"recentSearches",A.x("Recent searches"),"refund",A.x("Refund"),"removeAuthorize",A.x("Deauthorize"),"save",A.x("Save"),"search",A.x("Search"),"selectContactOrAddress",A.x("Choose a address or contact"),"send",A.x("Send"),"sendTo",A.x("Send to"),"sendToContact",A.x("Send to contact"),"settings",A.x("Settings"),"show",A.x("Show"),"slippage",A.x("Slippage"),"slippageOver",B.bd0(),"sortBy",A.x("SORT BY"),"swap",A.x("Swap"),"swapDisclaimer",A.x("Services provided by MixSwap"),"swapType",A.x("Swap type"),"symbol",A.x("Symbol"),"tagHint",A.x("Tag"),"time",A.x("Time"),"to",A.x("To"),"totalBalance",A.x("Total Balance"),"trace",A.x("Trace"),"transaction",A.x("Transaction"),"transactionChecking",A.x("Checking"),"transactionDone",A.x("Done"),"transactionFee",A.x("Transaction fee:"),"transactionFeeTip",A.x("Charged by third party service provider. US users are charged 2.9% + 30c of the transaction amount with a minimum charge of $5. International users are charged 3.9% + 30c of the transaction amount with a minimum charge of $5."),"transactionHash",A.x("Transaction Hash"),"transactionPhase",A.x("Transaction phase"),"transactionTrading",A.x("Trading"),"transactions",A.x("Transactions"),"transactionsAssetKeyWarning",A.x("Asset key is NOT a deposit address!"),"transactionsId",A.x("Transaction Id"),"transactionsStatus",A.x("Status"),"transactionsType",A.x("Transaction Type"),"transfer",A.x("Transfer"),"transferDetail",A.x("Transfer details"),"undo",A.x("UNDO"),"unpaid",A.x("Unpaid"),"waitingActionDone",A.x("Waiting action done..."),"walletTransactionCurrentValue",B.bcS(),"walletTransactionThatTimeNoValue",A.x("value then N/A"),"walletTransactionThatTimeValue",B.bcT(),"wireServiceTip",A.x("This service is provided by Wyre. We act as a conduit only and do not charge additional fees."),"withdrawal",A.x("Withdrawal"),"withdrawalMemoHint",A.x("Memo (Optional)"),"wyreServiceStatement",A.x("Service statement")],y.g,y.a)},
+U2:function U2(d){this.a=d}},A,C
+a.setFunctionNamesIfNecessary([B])
+B=a.updateHolder(c[3],B)
+A=c[5]
+C=c[0]
+B.U2.prototype={
+gWC(){return"en"},
+gJa(){return this.a}}
+var z=a.updateTypes(["f(@)","f(@,@)","ak<f,hM>(@)"]);(function installTearOffs(){var x=a._static_1,w=a._static_2
+x(B,"bcQ","b3P",0)
+x(B,"bcR","b3R",0)
+x(B,"bcU","b3X",0)
+x(B,"bcV","b3Y",0)
+x(B,"bcW","b3Z",0)
+x(B,"bcX","b40",0)
+x(B,"bcY","b42",0)
+x(B,"bcZ","b44",0)
+w(B,"bd_","b46",1)
+x(B,"bd0","b48",0)
+x(B,"bcS","b3S",0)
+x(B,"bcT","b3U",0)
+x(B,"bcP","aSS",2)})();(function inheritance(){var x=a.inherit
+x(B.U2,A.ny)})()
+C.aHC(b.typeUniverse,JSON.parse('{"U2":{"ny":[]}}'))
+var y={a:C.aa("hM"),g:C.aa("f")};(function lazyInitializers(){var x=a.lazyFinal
+x($,"biI","b_W",()=>new B.U2(B.aSS(B.bcP())))})()}
+$__dart_deferred_initializers__["tMcss/z0RJo+UA0ccDBLfKuLo5A="] = $__dart_deferred_initializers__.current
---
+++ main.dart.js_2.part.js
@@ -0,0 +1,24 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A={
+x(d){return new A.akD(d)},
+ny:function ny(){},
+akD:function akD(d){this.a=d},
+bbr(d,e,f){if(d!=="")return d
+return e}},B
+a.setFunctionNamesIfNecessary([A])
+A=a.updateHolder(c[5],A)
+B=c[0]
+A.ny.prototype={
+amF(d,e,f,g,h,i){var y=A.bbr(f,d,h),x=this.gJa().h(0,y)
+if(x==null)return d
+else return B.aMO(x,g,null)},
+h(d,e){return this.gJa().h(0,e)},
+j(d){return this.gWC()}}
+var z=a.updateTypes([])
+A.akD.prototype={
+$0(){return this.a},
+$S:11};(function inheritance(){var y=a.inherit
+y(A.ny,B.H)
+y(A.akD,B.C_)})()
+B.aHC(b.typeUniverse,JSON.parse("{}"))}
+$__dart_deferred_initializers__["Ie+vcOq8EEPaiaE9K5cfBYCTrL4="] = $__dart_deferred_initializers__.current
